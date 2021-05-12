35518fce7de32658714246770a4541518b076b96
Aleksandra Cynk <305284@student.agh.edu.pl>
2021-05-12 18:10:15 +0200

    Refractor and navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { createStackNavigator } from '@react-navigation/stack';
+
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import TabOneScreen from "./screens/TabOneScreen";
+import TabTwoScreen from "./screens/TabTwoScreen";
+import DetailsScreen from "./screens/DetailsScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -13,10 +21,13 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <NavigationContainer>
+      <Stack.Navigator initialRouteName="One">
+        <Stack.Screen name="One" component={TabOneScreen} />
+        <Stack.Screen name="Two" component={TabTwoScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
     );
   }
 }
