395214cf0e8672e0bbed87b0dbae801821afe50f
Aleksandra Cynk <305284@student.agh.edu.pl>
2021-05-14 17:37:04 +0200

    error detection

--- fetchApi.tsx
+++ fetchApi.tsx
@@ -1,19 +1,31 @@
+import NotFoundScreen from "./screens/NotFoundScreen";
 import { Vehicle, VehicleList } from "./types";
 
+function handle_error() {}
 const fetchAPI = {
   getVehiclesListAndCount: async function (
     page: number
   ): Promise<[VehicleList, number]> {
-    let url = `https://swapi.dev/api/vehicles/?page=${page}`;
-    const response = await fetch(url);
-    const json = await response.json();
-    return [json.results, json.count];
+    try {
+      let url = `https://swapi.dev/api/vehicles/?page=${page}`;
+      const response = await fetch(url);
+      const json = await response.json();
+      return [json.results, json.count];
+    } catch (err) {
+      handle_error();
+    }
+    return [[], 0];
   },
   nextPageVehiclesExists: async function (page: number) {
-    let url = `https://swapi.dev/api/vehicles/?page=${page}`;
-    const response = await fetch(url);
-    const json = await response.json();
-    return json.next != null;
+    try {
+      let url = `https://swapi.dev/api/vehicles/?page=${page}`;
+      const response = await fetch(url);
+      const json = await response.json();
+      return json.next != null;
+    } catch (err) {
+      handle_error();
+    }
+    return 0;
   },
   getVehicle: async function (id: number): Promise<Vehicle> {
     let url = `https://swapi.dev/api/vehicles/${id}`;
