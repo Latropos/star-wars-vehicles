58a09de850106b37c9e4809d88208f0a662b6123
Aleksandra Cynk <305284@student.agh.edu.pl>
2021-05-16 11:25:16 +0200

    Filtering vehicles working

--- screens/VehicleDetailsScreen.tsx
+++ screens/VehicleDetailsScreen.tsx
@@ -5,85 +5,92 @@ import { Vehicle } from "../types";
 import fetchAPI from "../fetchApi";
 
 interface DetailsItemProps {
-  name: string;
-  value: string;
+    name: string;
+    value: string;
 }
 
 const DetailsItem = ({ name, value }: DetailsItemProps) => (
-  <Text style={styles.listItem}>
-    {name}: {value}
-  </Text>
+    <Text style={styles.listItem}>
+        {name}: {value}
+    </Text>
 );
 
 const emptyVehicle = (): Vehicle => ({
-  name: "---",
-  model: "---",
-  manufacturer: "",
-  cost_in_credits: "",
-  max_atmosphering_speed: "",
-  passengers: "",
+    name: "---",
+    model: "---",
+    manufacturer: "",
+    cost_in_credits: "",
+    max_atmosphering_speed: "",
+    passengers: "",
+    length: 0,
+    crew: 0,
 });
 
-export default function VehicleDetailsScreen({ navigation, route }) {
-  const [isLoading, setLoading] = useState(true);
-  //TODO
-  const [vehicle, setVehicle] = useState<Vehicle | undefined>(emptyVehicle);
+export default function VehicleDetailsScreen({ route }) {
+    const [isLoading, setLoading] = useState(true);
+    //TODO
+    const [vehicle, setVehicle] = useState<Vehicle | undefined>(emptyVehicle);
 
-  useEffect(() => {
-    async function loadVehicle() {
-      let id: number = route.params.id;
+    useEffect(() => {
+        async function loadVehicle() {
+            let id: number = route.params.id;
 
-      const json = await fetchAPI.getVehicle(id);
-      setVehicle(json);
-    }
-    loadVehicle();
-    setLoading(false);
-  }, []);
+            const json = await fetchAPI.getVehicle(id);
+            setVehicle(json);
+        }
+        loadVehicle();
+        setLoading(false);
+    }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Wehicle details:</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
+    return (
+        <View style={styles.container}>
+            <View
+                style={styles.separator}
+                lightColor="#eee"
+                darkColor="rgba(255,255,255,0.1)"
+            />
 
-      {isLoading ? (
-        <ActivityIndicator />
-      ) : (
-        <View>
-          <DetailsItem name="Name" value={vehicle.name} />
-          <DetailsItem name="Model" value={vehicle.model} />
-          <DetailsItem name="Manufacturer" value={vehicle.manufacturer} />
-          <DetailsItem name="Cost in credits" value={vehicle.cost_in_credits} />
-          <DetailsItem
-            name="Max atm speed"
-            value={vehicle.max_atmosphering_speed}
-          />
-          <DetailsItem name="Passengers" value={vehicle.passengers} />
+            {isLoading ? (
+                <ActivityIndicator />
+            ) : (
+                <View>
+                    <DetailsItem name="Name" value={vehicle.name} />
+                    <DetailsItem name="Model" value={vehicle.model} />
+                    <DetailsItem
+                        name="Manufacturer"
+                        value={vehicle.manufacturer}
+                    />
+                    <DetailsItem
+                        name="Cost in credits"
+                        value={vehicle.cost_in_credits}
+                    />
+                    <DetailsItem
+                        name="Max atm speed"
+                        value={vehicle.max_atmosphering_speed}
+                    />
+                    <DetailsItem name="Passengers" value={vehicle.passengers} />
+                </View>
+            )}
         </View>
-      )}
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-  listItem: {
-    fontSize: 20,
-  },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    title: {
+        fontSize: 40,
+        fontWeight: "bold",
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: "80%",
+    },
+    listItem: {
+        fontSize: 20,
+    },
 });
